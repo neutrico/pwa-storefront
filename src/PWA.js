05e660c7ed5df91e47153f1d60454dbeedf1c2df
John Doe <marcin.antczak@neutrico.eu>
2021-07-23 00:25:59 +0200

    0.3.0

--- src/PWA.js
+++ src/PWA.js
@@ -2,29 +2,11 @@ import React, { useEffect, useMemo, useRef, useState } from 'react'
 import PWAContext from './PWAContext'
 import PropTypes from 'prop-types'
 import ErrorBoundary from './ErrorBoundary'
-import { makeStyles } from '@material-ui/core/styles'
-import Router from 'next/router'
 import LinkContextProvider from './link/LinkContextProvider'
 import useSimpleNavigation from './router/useSimpleNavigation'
 import './hooks/useTraceUpdate'
 import './profile'
-
-export const styles = theme => ({
-  '@global': {
-    body: {
-      '-webkit-tap-highlight-color': 'transparent',
-    },
-    a: {
-      color: theme.palette.primary.main,
-      textDecoration: 'underline',
-    },
-  },
-})
-
-const useStyles = makeStyles(styles, { name: 'RSFPWA' })
-
 export default function PWA({ children, errorReporter }) {
-  useStyles()
   const thumbnail = useRef(null)
   const [offline, setOffline] = useState(typeof navigator !== 'undefined' && !navigator.onLine)
 
