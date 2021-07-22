1a0f7a6e19b8fdb3c37cacfc5b652131d554d7a1
John Doe <marcin.antczak@neutrico.eu>
2021-07-22 23:48:45 +0200

    feature: storybook removed

--- .storybook/stories/ToolbarButton.stories.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { Pets } from '@material-ui/icons'
-import ToolbarButton from '../../src/ToolbarButton'
-
-export default { title: 'ToolbarButton' }
-
-export const defaults = () => <ToolbarButton icon={<Pets />} label="Label" />
